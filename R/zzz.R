81025a37a509ac95eccb747c1aec4815365b296b
Steven Sanderson <spsanderson@gmail.com>
2023-01-21 19:00:02 +0000

    version 1.0.2

---
+++ R/zzz.R
@@ -0,0 +1,18 @@
+# On library attachment, print message to user.
+.onAttach <- function(libname, pkgname) {
+
+    msg <- paste0(
+        "\n",
+        "== Welcome to healthyR.data ===========================================================================",
+        "\nIf you find this package useful, please leave a star: ",
+        "\n   https://github.com/spsanderson/healthyR.data'",
+        "\n",
+        "\nIf you encounter a bug or want to request an enhancement please file an issue at:",
+        "\n   https://github.com/spsanderson/healthyR.data/issues",
+        "\n",
+        "\nThank you for using healthyR.data"
+    )
+
+    packageStartupMessage(msg)
+
+}
